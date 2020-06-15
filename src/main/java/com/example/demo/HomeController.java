9bfb88d135c7ecab29cbdbbb8dc5dadf64fa96c0
keykode20 <omryemilyohassan@gmail.com>
2020-06-15 18:55:31 +0200

    hello

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,13 +4,19 @@ import com.example.demo.model.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.apache.log4j.Logger;
+
 @RestController
 public class HomeController {
 
+    final static Logger logger = Logger.getLogger(HomeController.class);
+
     @RequestMapping
     public Model hello(){
+        logger.info("starting home controller");
         Model model = new Model();
         model.setName("hey");
+        logger.info(model);
         return model;
     }
 
