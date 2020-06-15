4be0d95df7fceb73e3f9af9412763641c22e5682
keykode20 <omryemilyohassan@gmail.com>
2020-06-15 19:45:19 +0200

    hello

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -5,20 +5,25 @@ import org.apache.log4j.BasicConfigurator;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.apache.log4j.Logger;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Logger;
 
 @RestController
 public class HomeController {
 
-    final static Logger logger = Logger.getLogger(HomeController.class);
+    final static Logger logger = Logger.getLogger(HomeController.class.getName());
 
     @RequestMapping
-    public Model hello(){
+    public Model hello() throws IOException {
         BasicConfigurator.configure();
+        Handler handler = new FileHandler("emilio.log");
+        logger.addHandler(handler);
         logger.info("starting home controller");
         Model model = new Model();
         model.setName("hey");
-        logger.info(model);
+        logger.info(model.getName());
         return model;
     }
 
